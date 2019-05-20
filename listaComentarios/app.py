51601172fb06de024404ba5c565360288ba9abec
Ramon Moraes <ramon.moraes94@gmail.com>
2019-05-20 20:19:36 -0300

    add cors on python

--- listaComentarios/app.py
+++ listaComentarios/app.py
@@ -1,17 +1,21 @@
-
-from flask import jsonify, Flask
 import json
+from flask import jsonify, Flask
+from flask_cors import CORS, cross_origin
 
 data = {}
 with open("./data.json") as f:
         data = json.load(f)
+
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route("/", methods=["GET"])
+@cross_origin()
 def api_doc():
         return jsonify(data)
 
 @app.route("/doc/<int:doc_id>", methods=["GET"])
+@cross_origin()
 def get_doc(doc_id):
         comment_list = data.get("data")
         comments = [comment['text'] for comment in comment_list if comment['from_doc'] == doc_id]
